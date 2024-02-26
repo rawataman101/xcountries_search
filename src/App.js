8a809e96a39594cba33c8a837b548d54f78b2e29
Aman Rawat <rawataman101@gmail.com>
2024-02-26 18:08:22 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [countries, setCountries] = useState([]);
+  const [searchedCountries, setSearchCountries] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((err) => console.log("Error fetching API"));
+  });
+
+  useEffect(() => {
+    if (search) {
+      setSearchCountries([
+        ...countries.filter((country) => country.name.common.includes(search)),
+      ]);
+    }
+  }, [search, countries]);
+  const containerStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  };
+  const cardStyle = {
+    width: "200px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "1px solid #808080",
+    padding: "10px",
+    margin: "10px",
+    borderRadius: "10px",
+  };
+  const cardImgStyle = {
+    objectfit: "contain",
+    width: "100px",
+    height: "100px",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="search__wrapper">
+        <input
+          type="text"
+          placeholder="Search for countries..."
+          value={search}
+          onChange={handleSearch}
+        />
+      </div>
+      <div style={containerStyle}>
+        {search &&
+          searchedCountries.map((country) => (
+            <div style={cardStyle}>
+              <img
+                src={country.flags.png}
+                alt={`Flag of ${country.name.common}`}
+                style={cardImgStyle}
+              />
+              <h2 style={{ textAlign: "center" }}>{country.name.common}</h2>
+            </div>
+          ))}
+        {!search &&
+          countries.map((country) => (
+            <div style={cardStyle}>
+              <img
+                src={country.flags.png}
+                alt={`Flag of ${country.name.common}`}
+                style={cardImgStyle}
+              />
+              <h2 style={{ textAlign: "center" }}>{country.name.common}</h2>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
-
-export default App;
